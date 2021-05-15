dcd3b446b4827280b24201d9a7c47a05faf9b578
LuisFelipeMatos <60949831+LuisFelipeMatos@users.noreply.github.com>
2021-05-15 18:01:23 -0300

    Add files via upload

---
+++ pages/cadastro/new.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-7 d-flex vh-100 justify-content-center">
+        <div class="col-md-5 align-self-center">
+          <h1>Cadastro</h1>
+          <p>Informe seus dados</p>
+
+          <b-form @submit.prevent="cadastro">
+            <b-form-group>
+              <b-form-input
+                v-model="usuario.email"
+                type="email"
+                placeholder="E-mail"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+             <b-form-group>
+              <b-form-input
+                v-model="usuario.nome"
+                type="text"
+                placeholder="Nome"
+                required
+              ></b-form-input>
+            </b-form-group>
+             
+             
+
+            <b-form-group>
+              <b-form-input
+                v-model="usuario.senha"
+                type="password"
+                placeholder="Senha"
+                required
+              ></b-form-input>
+              
+            </b-form-group>
+
+            	
+            <b-button block type="submit" variant="primary">Cadastrar</b-button>
+          </b-form>
+        </div>
+      </div>
+      <div class="col-md-5 vh-100 cover"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario:{
+      email: null,
+      nome: null,
+      senha: null
+    }
+  };
+  },
+  methods: {
+    async cadastro() {
+      this.$store.dispatch("usuario/add", this.usuario);
+       
+    }
+ }
+};
+
+</script>
+
+<style>
+
+.cover {
+  background: url("https://url.gratis/wftZc") center center;}
+</style>
---
+++ pages/nota/new.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card bg-warning my-5">
+    <div class="card-body">
+      <b-form @submit.prevent="adicionar" autocomplete="off">
+        <b-form-group>
+          <b-form-input
+            class="bg-warning border-0 titulo text-dark"
+            v-model="nota.titulo"
+            type="text"
+            size="lg"
+            placeholder="Título"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group>
+          <b-form-textarea
+            class="bg-warning border-0 text-dark"
+            v-model="nota.descricao"
+            placeholder="Descrição"
+            :required="true"
+            rows="10"
+            size="lg"
+            :autofocus="true"
+          ></b-form-textarea>
+        </b-form-group>
+
+       <b-button type="button" variant="warning">
+        <b-icon icon="check2-square" />
+      </b-button>
+      <b-button type="button" variant="warning">
+        <b-icon icon="tag" />
+      </b-button>
+      <b-button type="submit" variant="dark" >
+       Adicionar 
+      </b-button>
+      </b-form>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "home",
+  data() {
+    return {
+      nota: {
+        titulo: null,
+        descricao: null,
+        checklists: [],
+        tags: []
+      }
+    };
+  },
+  methods:{
+    async adicionar(){
+      this.$store.dispatch("nota/add", this.nota);
+    }
+  }
+};
+</script>
+
+<style>
+.titulo {
+  font-weight: 600;
+}
+</style>
